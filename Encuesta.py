f6315335fef354d7bb01aab5f54d7ba9ea51caaf
Derek <derekmatul@gmail.com>
2025-08-16 10:38:11 -0600

    Encuesta commit2

--- Encuesta.py
+++ Encuesta.py
@@ -25,3 +25,36 @@ class PreguntasOpcionMultiple(Pregunta):
             self.respuestas.append(self.opciones[indice])
         else:
             print("Opcion no encontrada")
+    
+    def mostrar_resultado(self):
+        print(f"Pregunta de Opcion multiple: {self.texto}\nOpciones:")
+        for i in range(len(self.opciones)):
+            print(f"{i + 1}. {self.opciones[i]}")
+        super().mostrar_resultado()
+
+class PreguntaTextoLibre(Pregunta):
+    def registrar_respuesta(self, texto):
+        self.texto.append(texto)
+
+    def mostrar_resultado(self):
+        print(f"Pregunta de texto libre: {self.texto}")
+        return super().mostrar_resultado()
+
+class Encuesta:
+    def _init_(self, titulo):
+        self.titulo = titulo
+        self.preguntas = []
+
+    def agregar_pregunta(self, pregunta):
+        self.preguntas.append(pregunta)
+
+    def mostrar_preguntas(self):
+        print(f"Encuesta: {self.titulo}\nPreguntas:")
+        for i in range(len(self.preguntas)):
+           print(f"{i + 1}. {self.preguntas[i]}")
+
+    def responder_encuesta(self):
+        print(f"== Encuensta: {self.titulo} ==")
+        for preguntas in self.preguntas:
+            if isinstance(preguntas, PreguntasOpcionMultiple):
+                print(f"------\n{preguntas.texto}")
