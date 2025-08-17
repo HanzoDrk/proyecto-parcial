aa1fd97e9ab5ca5586697c3d70044195bb711d83
Fernandinh0o <luifernandocastro17@gmail.com>
2025-08-17 03:49:37 -0600

    Agregar clase Sistema con menú, crear, responder y mostrar resultados

--- Proyecto con correciones.py
+++ Proyecto con correciones.py
@@ -81,3 +81,106 @@ class Encuesta:
             preguntas.mostrar_resultados()
 
 #Perimite crear las encuestas, administrarlas y es el encargado de mostrastrar el menu.
+
+class Sistema:
+    def __init__(self):
+        self.encuestas = []
+            
+    def crear_encuesta(self):
+        titulo = input("Titulo de la encuesta: ")
+        encuesta = Encuesta(titulo)
+
+        while True:
+            print("== Tipo de Preguntas ==")
+            tipo = input("0. Salir | 1. Opcion Multiple | 2. Texto Libre\n")
+            if tipo == "1":
+                texto = input("Escriba la Pregunta: ")
+                opciones = []
+                while True:
+                    opcion = input("Agrege una opcion <<Escriba 0 para terminar>>: ")
+                    if opcion == "0":
+                        break
+                    opciones.append(opcion)
+                encuesta.agregar_pregunta(PreguntasOpcionMultiple(texto, opciones))
+                print("------")
+
+            elif tipo == "2":
+                texto = input("Escriba la Pregunta: ")
+                encuesta.agregar_pregunta(PreguntaTextoLibre(texto))
+                print("------")
+
+            elif tipo == "0": #Me falto agregar la opcion de salir del bucle
+                break 
+            else:
+                print("Opcion no valida. Seleccione una de las 3 opciones.")
+
+        #movi estas dos lineas un espacio para atras ya que se ejecutava todas las veces que se crea una pregunta y no solo una vez.
+        print("------")
+        self.encuestas.append(encuesta)
+        print("Encuesta Creada :D")
+        
+    def responder_encuesta(self):
+        if not self.encuestas:
+            print("No hay encuestas")
+            return
+        self.mostrar_encuestas()
+        while True:
+            try:
+                inx = int(input("Seleccione una encuesa: "))-1
+                if 0 <= inx < len(self.encuestas):
+                    self.encuestas[inx].responder_encuesta()
+                    break
+                else:
+                    print("La opcion que intenta elegir no existe.")
+            except ValueError:
+                print("Entrada invalida. Por favor, ingrese un numero.")
+
+    def mostrar_resultados(self):
+        if not self.encuestas:
+            print("No hay encustas para mostrar")
+            return
+        self.mostrar_encuestas()
+
+        while True:
+            try:
+                inx = int(input("Seleccione una encuesa: "))-1
+                if 0 <= inx < len(self.encuestas):
+                    self.encuestas[inx].mostrar_resultados()
+                    break
+                else:
+                    print("La opcion que intenta elegir no existe.")
+            except ValueError:
+                print("Ingrese un numero valido.")
+
+    def mostrar_encuestas(self):
+        print(f"=== Encuestas Disponibles ===")
+        for i in range(len(self.encuestas)):
+            print(f"{i + 1}. {self.encuestas[i].titulo}")
+
+    def menu(self):
+        while True:
+            print("\n=-= Sistema de Encuestas =-=\n0. Salir\n1. Crear encuesta\n2. Responder encuesta\n3. Mostrar resultado")
+            opciones = input("selecione una opcion: ")
+
+            if opciones == "1":
+                self.crear_encuesta()
+            elif opciones == "2":
+                self.responder_encuesta()
+            elif opciones == "3":
+                self.mostrar_resultados()
+                #El metodo esta mal escrito, ya que es con s"
+                #Seria mostrar_resultados()
+            elif opciones == "0":
+                print("\nSaliendo del sistema de encuestas. Adios!!!")
+                break
+            else:
+                print("Seleccione una opcion del sistema de encuestas.")
+
+p = Sistema()
+p.menu()
+
+'''
+Errores al tratar de ejecutarlo.
+problema con los constructores. los tenia asi = _init_.
+Error al unificar el nombres de los metodos, use metodos en singular y plural
+'''
