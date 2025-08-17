b9933a6685f6b95b07304c1835539e4f8b0a56e1
Fernandinh0o <luifernandocastro17@gmail.com>
2025-08-17 03:49:11 -0600

    Agregar clase Encuesta con agregar, mostrar y responder preguntas

--- Proyecto con correciones.py
+++ Proyecto con correciones.py
@@ -43,3 +43,41 @@ class PreguntaTextoLibre(Pregunta):
     def mostrar_resultados(self):
         print(f"Pregunta de texto libre: {self.texto}")
         super().mostrar_resultados()
+
+#Clase Encuesta, Muestra las preguntas, permite agregar preguntas y responder. Encargado del flujo de la encuesta
+class Encuesta:
+    def __init__(self, titulo):
+        self.titulo = titulo
+        self.preguntas = []
+
+    def agregar_pregunta(self, pregunta): #Metodo para agregar preguntas
+        self.preguntas.append(pregunta)
+
+    def mostrar_preguntas(self):
+        print(f"Encuesta: {self.titulo}\nPreguntas:")
+        for i in range(len(self.preguntas)):
+           print(f"{i + 1}. {self.preguntas[i]}")
+
+    def responder_encuesta(self):
+        print(f"== Encuensta: {self.titulo} ==")
+        for preguntas in self.preguntas:
+            if isinstance(preguntas, PreguntasOpcionMultiple):
+                print(f"------\n{preguntas.texto}")
+                for i in range(len(preguntas.opciones)): #cambie el metodo ya que recorria el texto
+                    print(f"{i + 1}. {preguntas.opciones[i]}") #lo mismo, cambie texto por opciones
+                try:
+                    eleccion = int(input("Seleccione una opcion: ")) -1
+                    preguntas.registrar_respuesta(eleccion)
+                except ValueError:
+                    print("Error. entrada invalida seleccione una opcion.")
+            elif isinstance(preguntas, PreguntaTextoLibre):
+                respuesta = input(f"{preguntas.texto}\nRespuesta: ")
+                preguntas.registrar_respuesta(respuesta)
+
+    def mostrar_resultados(self):
+        #Simplemente un separador
+        print(f"=== Resultados de la Encuesta {self.titulo} ===")
+        for preguntas in self.preguntas:
+            preguntas.mostrar_resultados()
+
+#Perimite crear las encuestas, administrarlas y es el encargado de mostrastrar el menu.
